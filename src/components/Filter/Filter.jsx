b7c999feecc8d9ebe3d3a92848167dc053d45c05
Yurii Konoval <yuriioleksandrovichkonoval@gmail.com>
2021-01-14 23:18:23 +0200

    react import in components

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,5 +1,6 @@
-import s from './Filter.module.css';
+import React from 'react';
 import PropTypes from 'prop-types';
+import s from './Filter.module.css';
 
 const Filter = ({ value, onChange }) => (
   <label className={s.label}>
